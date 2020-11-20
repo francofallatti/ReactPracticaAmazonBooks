2a77ad84cd696974875b47ed35c141c7ed72f8b3
francofallatti <66625402+francofallatti@users.noreply.github.com>
2020-11-20 04:42:48 -0300

    header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
+import Menu from './Menu';
+import List from './List';
 
 function App() {
-  return <div>Hola mundo </div>;
+  return (
+    <div className='app'>
+      <Menu title='amazon' />
+      <List />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/List.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+function List(props) {
+  return <h1>hola mundo</h1>;
+}
+export default List;
